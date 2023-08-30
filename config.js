6f7617bda4ab7cac3d6bb830deb1b1c00234cd69
Tharushajauarathna <143621567+Tharushajauarathna@users.noreply.github.com>
2023-08-30 19:25:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94714344937', 'Tharu Official', true],
+  ['94714344937', 'Tharu Official', true], 
   [''] 
 ] //Number of owners
 
